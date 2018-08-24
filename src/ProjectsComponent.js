e25d124a81097146af3aadd2220f95c2d0874051
Uppun <TheSuperBro@gmail.com>
2018-08-24 09:06:18 -0400

    added a footer

--- src/ProjectsComponent.js
+++ src/ProjectsComponent.js
@@ -23,12 +23,24 @@ const ProjectItems = [
 
 export default class Projects extends Component {
     render() {
+        const footerString = "If you'd like to see more, check out my github here!";
+        const githubURL = "https://github.com/Uppun";
         return(
             <div className='project-component'>
-                {ProjectItems.map(project => {
-                    const {title, description} = project;
-                    return <ProjectItem title={title} description={description} />
-                })}
+                <h1>Projects</h1>
+                <div className='project-listing'>
+                    {ProjectItems.map(project => {
+                        const {title, description} = project;
+                        return <ProjectItem title={title} description={description} />
+                    })}
+                </div>
+                <div className='project-component-footer'>
+                    <a href={githubURL} target="_blank">
+                        <div className='footer-string'>
+                            {footerString}
+                        </div>
+                    </a>
+                </div>
             </div>
         )
     }
