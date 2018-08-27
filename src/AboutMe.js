47e21c60354199a86f200758c8ef778582511458
Uppun <TheSuperBro@gmail.com>
2018-08-27 05:22:19 -0400

    why is it overlapping

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 import './portfolio.css';
+import backgrounds from './assets/backgrounds';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 export default class AboutMe extends Component {
+    state = {currentBackground: 0};
+
+    componentDidMount() {
+        this.interval = setInterval(this.updateBackground, 10000);
+    }
+
+    updateBackground = () => {
+        this.setState({
+            currentBackground: (this.state.currentBackground + 1) % backgrounds.length,
+        });
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
     render() {
         return(
-            <div className="about-me-component">
-                <div className="header-image">
-
-                </div>
-                <div className="about-me-info">
-                </div>
+            <div className='background-pages'>
+                <TransitionGroup className='background-slide'>
+                    {backgrounds.map((background, index) => {
+                        if (index === this.state.currentBackground) {
+                            return (
+                                <CSSTransition key={index} timeout={2000} classNames='switch'>
+                                    <BackgroundPage style={{backgroundImage: 'url(' + backgrounds[index] + ')'}} />
+                                </CSSTransition>
+                            )
+                        } else {
+                            return null;
+                        }
+                    })}
+                </TransitionGroup>
             </div>
-        )
+        );
+    }
+}
+
+class BackgroundPage extends Component {
+    render() {
+        return (
+            <div className='landing-background' style={{backgroundImage: this.props.style.backgroundImage}} />
+        );
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import ProjectsComponent from './ProjectsComponent';
+import AboutMe from './AboutMe';
 
 class App extends Component {
   render() {
     return (
       <div className='portfolio-components'>
-        <ProjectsComponent />
+        <div className='about-me'>
+          <AboutMe />
+        </div>
+        <div className='my-projects'>
+          <ProjectsComponent />
+        </div>
       </div>
     );
   }
