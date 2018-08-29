dd1238d3b6b487064e3a7ad89cfe7c7b33b73c5d
Uppun <TheSuperBro@gmail.com>
2018-08-29 06:21:33 -0400

    I hate css transitions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import ProjectsComponent from './ProjectsComponent';
 import AboutMe from './AboutMe';
+import Influences from './Influences';
 
 class App extends Component {
   render() {
     return (
-      <div className='portfolio-components'>
+      <Influences />
+      /*<div className='portfolio-components'>
         <div className='about-me'>
           <AboutMe />
         </div>
         <div className='my-projects'>
           <ProjectsComponent />
         </div>
-      </div>
+      </div>*/
     );
   }
 }
