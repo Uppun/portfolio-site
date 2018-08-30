19b14d981131d482c6ddf84c3fad9624507bd8b3
Uppun <TheSuperBro@gmail.com>
2018-08-30 05:08:34 -0400

    influences page spruced up a bit, fade works now

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import Influences from './Influences';
 class App extends Component {
   render() {
     return (
-      <Influences />
-      /*<div className='portfolio-components'>
+      <div className='portfolio-components'>
         <div className='about-me'>
           <AboutMe />
+        </div>
+        <div className='influences'>
+          <Influences />
         </div>
         <div className='my-projects'>
           <ProjectsComponent />
         </div>
-      </div>*/
+      </div>
     );
   }
 }
--- src/Influences.js
+++ src/Influences.js
@@ -1,39 +1,58 @@
 import React, { Component } from 'react';
 import './portfolio.css';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import testImg from './assets/testbun.png';
 
 export default class Influences extends Component {
   render() {
-            return(
+        return(
             <div className='influences-component'>
                     <div className='influences-component-section top-section'>
                         <div className='influences-component-content'>
                             <CSSTransition
-                                in={true} 
-                                timeout={1000}
-                                unmountOnExt={true}
+                                in={true}
+                                appear={true} 
+                                timeout={1000} 
                                 classNames='right-slide'
                             >
-                                <img src={testImg} alt='infimg' className='influence-image' />
+                                <p>
+                                    <img src={testImg} alt='infimg' className='influence-image' align='left' />
+                                    Lorem ipsum dolor sit amet, illum abhorreant an vel, ad vide porro nostrum nec. Saepe insolens vituperata ius at, vel elitr ludus ne, populo audiam ei nam. Essent audiam te mei, errem quidam ei nec. In option oblique vis, everti aperiam mei at, est ne velit oblique volutpat. Ex malorum eleifend nam, fugit voluptua ei vix.
+                                </p>
                             </CSSTransition>
                         </div>
                     </div>
                     <div className='influences-component-section middle-section'>
                         <div className='influences-component-content'>
-                            <CSSTransition timeout={1000} classNames='left-slide'>
-                                <img src={testImg} alt='infimg' className='influence-image' />
+                            <CSSTransition
+                                in={true}
+                                appear={true} 
+                                timeout={1000} 
+                                classNames='left-slide'
+                            >
+                                <p>
+                                    <img src={testImg} alt='infimg' className='influence-image' align='right' />
+                                    Lorem ipsum dolor sit amet, illum abhorreant an vel, ad vide porro nostrum nec. Saepe insolens vituperata ius at, vel elitr ludus ne, populo audiam ei nam. Essent audiam te mei, errem quidam ei nec. In option oblique vis, everti aperiam mei at, est ne velit oblique volutpat. Ex malorum eleifend nam, fugit voluptua ei vix.
+                                </p>
                             </CSSTransition>
                         </div>
                     </div>
                     <div className='influences-component-section bottom-section'>
                         <div className='influences-component-content'>
-                            <CSSTransition timeout={1000} classNames='right-slide'>
-                                <img src={testImg} alt='infimg' className='influence-image' />
+                            <CSSTransition
+                                in={true}
+                                appear={true} 
+                                timeout={1000} 
+                                classNames='right-slide'
+                            >
+                                <p>
+                                    <img src={testImg} alt='infimg' className='influence-image' align='left' />
+                                    Lorem ipsum dolor sit amet, illum abhorreant an vel, ad vide porro nostrum nec. Saepe insolens vituperata ius at, vel elitr ludus ne, populo audiam ei nam. Essent audiam te mei, errem quidam ei nec. In option oblique vis, everti aperiam mei at, est ne velit oblique volutpat. Ex malorum eleifend nam, fugit voluptua ei vix.
+                                </p>
                             </CSSTransition>
                         </div>
                     </div>
             </div>
         )
-}  
+    }  
 }
