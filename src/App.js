d2cc89ddd0fd8f287ddd646a1f4eb4ba6f3e7d23
Uppun <TheSuperBro@gmail.com>
2018-08-31 03:46:42 -0400

    we navbar now

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import ProjectsComponent from './ProjectsComponent';
 import AboutMe from './AboutMe';
 import Influences from './Influences';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className='portfolio-components'>
-        <div className='about-me'>
-          <AboutMe />
+        <div className='navbar-wrapper'>
+          <Navbar />
         </div>
-        <div className='influences'>
-          <Influences />
-        </div>
-        <div className='my-projects'>
-          <ProjectsComponent />
-        </div>
+        <Switch> 
+          <Route exact path='/' component={AboutMe} />
+          <Route path='/influences' component={Influences} />
+          <Route path='/projects' component={ProjectsComponent} />
+        </Switch>
       </div>
     );
   }
