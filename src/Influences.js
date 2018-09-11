cf65f5ebf427eacc111260f38d8a7137d7c1d9ea
Uppun <TheSuperBro@gmail.com>
2018-09-10 22:58:38 -0400

    influences page looks nifty

--- src/Influences.js
+++ src/Influences.js
@@ -1,58 +1,90 @@
 import React, { Component } from 'react';
 import './portfolio.css';
-import { CSSTransition } from 'react-transition-group';
 import testImg from './assets/testbun.png';
 import pkmImg from './assets/pkmblue.png';
 
 export default class Influences extends Component {
-  render() {
+    state = {
+        animatedSections: 0,
+    };
+
+    componentDidMount() {
+        this._sectionAnimationTimeout = setTimeout(this.animateNextSection, 500);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this._sectionAnimationTimeout);
+    }
+
+    animateNextSection = () => {
+        const animatedSections = this.state.animatedSections + 1;
+        this.setState({animatedSections});
+        if (animatedSections < 3) {
+          this._sectionAnimationTimeout = setTimeout(this.animateNextSection, 2000);
+        }
+      };
+
+    render() {
+        const {animatedSections} = this.state;
         return(
             <div className='influences-component'>
-                    <div className='influences-component-section top-section'>
-                        <div className='influences-component-content'>
-                            <CSSTransition
-                                in={true}
-                                appear={true} 
-                                timeout={1000} 
-                                classNames='right-slide'
-                            >
+                <div className='influences-component-section top-section'>
+                    <div className='influences-component-content'>
+                        <div className={animatedSections > 0 ? 'section-1 animate' : 'section-1'}>
+                            <div className='section-images'>
+                                <img class="i1" alt='cat1' src="https://placekitten.com/426/240" />
+                                <img class="i2" alt='cat2' src="https://placekitten.com/g/640/360" />
+                                <img class="i3" alt='cat3' src="https://placekitten.com/640/360" />
+                            </div>
+                            <div className='section-text'>
+                                <h1>
+                                Section 1
+                                </h1>
                                 <p>
-                                    <img src={pkmImg} alt='firstImg' className='influence-image' align='left' />
-                                    Lorem ipsum dolor sit amet, illum abhorreant an vel, ad vide porro nostrum nec. Saepe insolens vituperata ius at, vel elitr ludus ne, populo audiam ei nam. Essent audiam te mei, errem quidam ei nec. In option oblique vis, everti aperiam mei at, est ne velit oblique volutpat. Ex malorum eleifend nam, fugit voluptua ei vix.
+                                i love kittens aaaaaaaaaaaaaaaaaaaaaa what is this ooooooooo aaaaaaaaaa eeeeeeeeeeee
                                 </p>
-                            </CSSTransition>
+                            </div>
                         </div>
                     </div>
-                    <div className='influences-component-section middle-section'>
-                        <div className='influences-component-content'>
-                            <CSSTransition
-                                in={true}
-                                appear={true} 
-                                timeout={1000} 
-                                classNames='left-slide'
-                            >
+                </div>
+                <div className='influences-component-section middle-section'>
+                    <div className='influences-component-content'>
+                        <div className={animatedSections > 1 ? 'section-2 animate' : 'section-2'}>
+                            <div className='section-text'>
+                                <h1>
+                                Section 2
+                                </h1>
                                 <p>
-                                    <img src={testImg} alt='infimg' className='influence-image' align='right' />
-                                    Lorem ipsum dolor sit amet, illum abhorreant an vel, ad vide porro nostrum nec. Saepe insolens vituperata ius at, vel elitr ludus ne, populo audiam ei nam. Essent audiam te mei, errem quidam ei nec. In option oblique vis, everti aperiam mei at, est ne velit oblique volutpat. Ex malorum eleifend nam, fugit voluptua ei vix.
+                                phbbbbbbbbbbbbbtthese kittens sure are cute eeeeeeeeeeeeeeee wooooooo
                                 </p>
-                            </CSSTransition>
+                            </div>
+                            <div className='section-images'>
+                                <img class="i1" alt='cat1' src="https://placekitten.com/426/240" />
+                                <img class="i2" alt='cat2' src="https://placekitten.com/g/640/360" />
+                                <img class="i3" alt='cat3' src="https://placekitten.com/640/360" />
+                            </div>
                         </div>
                     </div>
-                    <div className='influences-component-section bottom-section'>
-                        <div className='influences-component-content'>
-                            <CSSTransition
-                                in={true}
-                                appear={true} 
-                                timeout={1000} 
-                                classNames='right-slide'
-                            >
+                </div>
+                <div className='influences-component-section bottom-section'>
+                    <div className='influences-component-content'>
+                        <div className={animatedSections > 2 ? 'section-3 animate' : 'section-3'}>
+                            <div className='section-images'>
+                                <img class="i1" alt='cat1' src="https://placekitten.com/426/240" />
+                                <img class="i2" alt='cat2' src="https://placekitten.com/g/640/360" />
+                                <img class="i3" alt='cat3' src="https://placekitten.com/640/360" />
+                            </div>
+                            <div className='section-text'>
+                                <h1>
+                                Section 3
+                                </h1>
                                 <p>
-                                    <img src={testImg} alt='infimg' className='influence-image' align='left' />
-                                    Lorem ipsum dolor sit amet, illum abhorreant an vel, ad vide porro nostrum nec. Saepe insolens vituperata ius at, vel elitr ludus ne, populo audiam ei nam. Essent audiam te mei, errem quidam ei nec. In option oblique vis, everti aperiam mei at, est ne velit oblique volutpat. Ex malorum eleifend nam, fugit voluptua ei vix.
+                                i love kittens
                                 </p>
-                            </CSSTransition>
+                            </div>
                         </div>
                     </div>
+                </div>
             </div>
         )
     }  
